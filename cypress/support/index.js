1bd64d52dee44ac12b06e94db8fb4e2dc1002cf3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-12-22 10:56:40 -0500

    add check

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,3 @@
 // https://github.com/bahmutov/cyclope
-import { savePageIfTestFailed } from 'cyclope'
-afterEach(savePageIfTestFailed)
+// import { savePageIfTestFailed } from 'cyclope'
+// afterEach(savePageIfTestFailed)
